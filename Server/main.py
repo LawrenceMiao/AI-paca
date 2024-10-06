472c7c6d0cd994909cad4b7797dbe67435bce5fe
Lawrence M <lawrencejunzh@gmail.com>
2024-10-05 21:47:45 -0400

    edit .env

--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from decouple import config
